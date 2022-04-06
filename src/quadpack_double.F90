58a5ecd88ee08226527f2745805a781a149037ac
jacobwilliams <jacobwilliams@users.noreply.github.com>
2022-04-06 00:50:08 +0000

    Deploying to gh-pages from @ jacobwilliams/quadpack@df4f4e70d53cb404a53d8aec06e8cb4016feab7d 🚀

--- src/quadpack_double.F90
+++
@@ -1,8 +0,0 @@
-module quadpack_double
-    !!
-    !!@note For this module, `wp` is `real64` (double precision).
-    !!
-    use iso_fortran_env, only: wp => real64
-#define MOD_INCLUDE=1
-#include "quadpack_generic.F90"
-end module quadpack_double
--- src/quadpack_single.F90
+++
@@ -1,8 +0,0 @@
-module quadpack_single
-    !!
-    !!@note For this module, `wp` is `real32` (single precision).
-    !!
-    use iso_fortran_env, only: wp => real32
-#define MOD_INCLUDE=1
-#include "quadpack_generic.F90"
-end module quadpack_single
